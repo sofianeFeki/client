c5bc471dca10af25401c60a4abd5193c96fccdbd
sofianeFeki <backofficekompar@gmail.com>
2023-03-05 18:07:44 +0100

    Update product.js

--- src/functions/product.js
+++ src/functions/product.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.REACT_APP_API;
+const API_BASE_URL = 'https://komparcrmserver.onrender.com/api'
 
 export const createContract = async (contract, authtoken) =>
   await axios.post(`${API_BASE_URL}/contract`, contract, {
