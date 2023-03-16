bdd4a588d21590c0973ab298987cf07744ec1570
sofianeFeki <backofficekompar@gmail.com>
2023-03-17 00:18:33 +0100

    Update product.js

--- src/functions/product.js
+++ src/functions/product.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://komparcrmserver.onrender.com/api'
+const API_BASE_URL = 'http://localhost:8000/api';
 
 export const createContract = async (contract, authtoken) =>
   await axios.post(`${API_BASE_URL}/contract`, contract, {
@@ -16,42 +16,54 @@ export const createContract = async (contract, authtoken) =>
 //     page,
 //   });
 
-export const getContractsAll = async (sort, order, page, filter, quickFilter) =>
+export const getContractsAll = async (filterValues) =>
   await axios.post('http://localhost:8000/api/contracts', {
-    sort,
-    order,
-    page,
-    filter,
-    quickFilter,
+    filterValues,
   });
 
-export const getContractsPagintationCursor = async (cursor, pageSize, page) =>
-  await axios.post(`${API_BASE_URL}/contractsPagintationCursor`, {
-    cursor,
-    pageSize,
-    page,
-  });
-
-export const qualityContractsPaginationCursor = async (
-  cursor,
+export const getContractsPagintationCursor = async (
+  sortOptions,
   pageSize,
   page
 ) =>
-  await axios.post(`${API_BASE_URL}/qualityContractsPaginationCursor`, {
-    cursor,
+  await axios.post(`${API_BASE_URL}/contractsPagintationCursor`, {
+    sortOptions,
     pageSize,
     page,
+  });
+
+export const qualityContractsPaginationCursor = async (page, pageSize) =>
+  await axios.post(`${API_BASE_URL}/qualityContractsPaginationCursor`, {
+    page,
+    pageSize,
   });
 
 export const getContractsFilters = async (
   filterOptions,
   quickFilterValue,
-  sortOptions
+  sortOptions,
+  page,
+  pageSize
 ) =>
   await axios.post(`${API_BASE_URL}/ContractsFilters`, {
     filterOptions,
     quickFilterValue,
     sortOptions,
+    page,
+    pageSize,
+  });
+
+export const getContractsAdvancedFilters = async (
+  filterValues,
+  quickFilterValue,
+  page,
+  pageSize
+) =>
+  await axios.post(`${API_BASE_URL}/ContractsAdvancedFilters`, {
+    filterValues,
+    quickFilterValue,
+    page,
+    pageSize,
   });
 
 export const getContractsCount = async () =>
@@ -79,6 +91,7 @@ export const updateContract = async (slug, values, authtoken) =>
       authtoken,
     },
   });
+
 export const updateContractData = async (slug, values, authtoken) =>
   await axios.put(`${API_BASE_URL}/contract/update/data/${slug}`, values, {
     headers: {
